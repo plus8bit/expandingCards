08cd6542e5abf57a7dd291dc3d57fe4fd3b8fe94
Nick <plus0bit@gmail.com>
2023-05-05 19:26:14 +0300

    Add panel click functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(el => {
+  el.addEventListener('click', () => {
+    removeActiveClass()
+    el.classList.add('active')
+  })
+})
+
+function removeActiveClass() {
+  panels.forEach(el => {
+    el.classList.remove('active')
+  })
+}
